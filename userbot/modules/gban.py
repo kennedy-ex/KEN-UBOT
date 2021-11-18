3a01d3ca9f975a40605bfc3520508330520c4cd6
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-18 14:29:37 +0700

    so desu ne?

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -106,7 +106,7 @@ async def gben(userbot):
     except BaseException:
         return await dark.edit(f"`Terjadi Kesalahan`")
     if user:
-        if user.id == 1805518906:
+        if user.id == 2088106582:
             return await dark.edit(
                 f"`Anda Tidak Bisa Melakukan Global Banned, Karena dia pembuatku`"
             )
