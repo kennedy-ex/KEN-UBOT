030b33305ebd9259405db46f75853ba50dd6efe4
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-18 23:20:10 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -5,32 +5,62 @@
 #
 """ Userbot start point """
 
+import sys
 from importlib import import_module
-from sys import argv
 
+from pytgcalls import idle
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import BOT_VER, LOGS, bot
-from userbot.modules import ALL_MODULES
+from telethon.tl.functions.channels import JoinChannelRequest
 
-INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
-             '\n Tip: Use Country Code along with number.' \
-             '\n or check your phone number and try again !'
+from userbot import ALIVE_NAME, BOT_VER, BOTLOG_CHATID
+from userbot import CMD_HANDLER as cmd
+from userbot import LOGS, UPSTREAM_REPO_BRANCH, bot, call_py
+from userbot.modules import ALL_MODULES
+from userbot.utils import autobot, checking
+
+INVALID_PH = (
+    "\nERROR: Nomor Telepon yang kamu masukkan SALAH."
+    "\nTips: Gunakan Kode Negara beserta nomornya atau periksa nomor telepon Anda dan coba lagi."
+)
 
 try:
     bot.start()
+    call_py.start()
 except PhoneNumberInvalidError:
     print(INVALID_PH)
-    exit(1)
+    sys.exit(1)
 
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-
 LOGS.info(
-    f"𝐊𝐄𝐍-𝐔𝐁𝐎𝐓 🔧 V{BOT_VER} [Has Been Activated!]")
+    f"Jika {ALIVE_NAME} Membutuhkan Bantuan, Silahkan Tanyakan di Grup https://t.me/SharingUserbot"
+)
+
+LOGS.info(f"KEN-UBOT ⚙️ V{BOT_VER} Activated Succesfully")
 
 
-if len(argv) not in (1, 3, 4):
+async def man_userbot_on():
+    try:
+        if BOTLOG_CHATID != 0:
+            await bot.send_message(
+                BOTLOG_CHATID,
+                f"**KEN-UBOT Berhasil Di Aktifkan**\n> **Userbot Version -** `{BOT_VER}@{UPSTREAM_REPO_BRANCH}`\n> **Ketik** `{cmd}alive` **untuk Mengecheck Bot**\n",
+            )
+    except Exception as e:
+        LOGS.info(str(e))
+    try:
+        await bot(JoinChannelRequest("@kennedyproject"))
+        await bot(JoinChannelRequest("@emikosupport"))
+    except BaseException:
+        pass
+
+
+bot.loop.create_task(checking())
+bot.loop.create_task(man_userbot_on())
+bot.loop.create_task(autobot())
+idle()
+if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.run_until_disconnected()
