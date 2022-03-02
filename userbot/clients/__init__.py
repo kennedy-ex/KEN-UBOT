dc28b5e6422cd9e9e27e346ba8871a8c018a8e36
Lil Kennedy 炎 <87056837+kennedy-ex@users.noreply.github.com>
2022-03-02 09:53:21 +0700

    Create __init__.py

---
+++ userbot/clients/__init__.py
@@ -0,0 +1,9 @@
+# Man - UserBot
+# Copyright (c) 2022 Man-Userbot
+# Credits: @mrismanaziz || https://github.com/mrismanaziz
+#
+# KEN-UBOT
+
+from .client_list import client_id, clients_list
+from .logger import ken_userbot_on
+from .startup import ken_client, multiclient
