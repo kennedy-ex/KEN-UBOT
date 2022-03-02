220df1e9984ff76b35ad85c021d7729e98bbc9ee
Lil Kennedy 炎 <87056837+kennedy-ex@users.noreply.github.com>
2022-03-02 10:25:44 +0700

    Update events.py

--- userbot/utils/events.py
+++ userbot/utils/events.py
@@ -2,14 +2,19 @@ import pybase64
 from telethon.tl.functions.channels import JoinChannelRequest as Get
 from telethon.tl.types import MessageEntityMentionName
 
-from userbot import bot, LOGS
+from userbot import APP2, APP3, APP4, APP5, bot
+
+from .logger import logging
+from .tools import edit_delete
+
+LOGS = logging.getLogger(__name__)
 
 
 async def get_user_from_event(
-    event, pantek=None, secondgroup=None, nogroup=False, noedits=False
+    event, kenevent=None, secondgroup=None, nogroup=False, noedits=False
 ):
-    if pantek is None:
-        pantek = event
+    if kenevent is None:
+        kenevent = event
     if nogroup is False:
         if secondgroup:
             args = event.pattern_match.group(2).split(" ", 1)
@@ -21,14 +26,11 @@ async def get_user_from_event(
             user = args[0]
             if len(args) > 1:
                 extra = "".join(args[1:])
-            if user.isnumeric() or (user.startswith("-")
-                                    and user[1:].isnumeric()):
+            if user.isnumeric() or (user.startswith("-") and user[1:].isnumeric()):
                 user = int(user)
             if event.message.entities:
                 probable_user_mention_entity = event.message.entities[0]
-                if isinstance(
-                        probable_user_mention_entity,
-                        MessageEntityMentionName):
+                if isinstance(probable_user_mention_entity, MessageEntityMentionName):
                     user_id = probable_user_mention_entity.user_id
                     user_obj = await event.client.get_entity(user_id)
                     return user_obj, extra
@@ -48,25 +50,29 @@ async def get_user_from_event(
             return user_obj, extra
         if event.reply_to_msg_id:
             previous_message = await event.get_reply_message()
-            if previous_message.from_id is None:
+            if previous_message.sender_id is None:
                 if not noedits:
-                    await pantek.edit(
-                        "**ERROR: Dia adalah anonymous admin!**"
+                    await edit_delete(
+                        kenevent, "**ERROR: Dia adalah anonymous admin!**", 60
                     )
                 return None, None
             user_obj = await event.client.get_entity(previous_message.sender_id)
             return user_obj, extra
         if not args:
             if not noedits:
-                await pantek.edit(
+                await edit_delete(
+                    kenevent,
                     "**Mohon Reply Pesan atau Berikan User ID/Username pengguna!**",
+                    60,
                 )
             return None, None
     except Exception as e:
         LOGS.error(str(e))
     if not noedits:
-        await pantek.edit(
+        await edit_delete(
+            kenevent,
             "**Mohon Reply Pesan atau Berikan User ID/Username pengguna!**",
+            60,
         )
     return None, None
 
@@ -75,10 +81,32 @@ async def checking():
     gocheck = str(pybase64.b64decode("QEx1bmF0aWMwZGU="))[2:13]
     checker = str(pybase64.b64decode("QFNoYXJpbmdVc2VyYm90"))[2:17]
     try:
-        await bot(Get(gocheck))
+        if bot:
+            await bot(Get(gocheck))
+            await bot(Get(checker))
     except BaseException:
         pass
     try:
-        await bot(Get(checker))
+        if KEN2:
+            await KEN2(Get(gocheck))
+            await KEN2(Get(checker))
+    except BaseException:
+        pass
+    try:
+        if KEN3:
+            await KEN3(Get(gocheck))
+            await KEN3(Get(checker))
+    except BaseException:
+        pass
+    try:
+        if KEN4:
+            await KEN4(Get(gocheck))
+            await KEN4(Get(checker))
+    except BaseException:
+        pass
+    try:
+        if KEN5:
+            await KEN5(Get(gocheck))
+            await KEN5(Get(checker))
     except BaseException:
         pass
