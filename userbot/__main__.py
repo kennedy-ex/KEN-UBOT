524d599a2ae6c2a6de4d03ade340467ca46eaacf
Lil Kennedy 炎 <87056837+kennedy-ex@users.noreply.github.com>
2022-03-02 09:46:15 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -3,34 +3,51 @@
 # Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
+# Copyright (C) 2021 TeamUltroid for autobot
+# Recode by @mrismanaziz
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+#
 """ Userbot start point """
 
+
+import sys
 from importlib import import_module
-from sys import argv
+from platform import python_version
 
-from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
-from userbot import BOT_VER, LOGS, bot
+from pytgcalls import __version__ as pytgcalls
+from pytgcalls import idle
+from telethon import version
+
+from userbot import BOT_TOKEN
+from userbot import BOT_VER as ubotversion
+from userbot import LOGS, bot
+from userbot.clients import ken_userbot_on, multiclient
 from userbot.modules import ALL_MODULES
-
-INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
-             '\n Tip: Use Country Code along with number.' \
-             '\n or check your phone number and try again !'
+from userbot.utils import autobot, checking
 
 try:
-    bot.start()
-except PhoneNumberInvalidError:
-    print(INVALID_PH)
-    exit(1)
+    for module_name in ALL_MODULES:
+        imported_module = import_module(f"userbot.modules.{module_name}")
+    client = multiclient()
+    total = 5 - client
+    LOGS.info(f"Total Clients = {total} User")
+    LOGS.info(f"Python Version - {python_version()}")
+    LOGS.info(f"Telethon Version - {version.__version__}")
+    LOGS.info(f"PyTgCalls Version - {pytgcalls.__version__}")
+    LOGS.info(f"KEN-UBOT Version - {ubotversion} [ ⚡ Is Now Online ⚡ ]")
+except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
+    pass
+except BaseException as e:
+    LOGS.info(str(e), exc_info=True)
+    sys.exit(1)
 
-for module_name in ALL_MODULES:
-    imported_module = import_module("userbot.modules." + module_name)
 
-
-LOGS.info(
-    f"𝐊𝐄𝐍-𝐔𝐁𝐎𝐓 🔧 V{BOT_VER} [Has Been Activated!]")
-
-
-if len(argv) not in (1, 3, 4):
+bot.loop.run_until_complete(checking())
+bot.loop.run_until_complete(ken_userbot_on())
+if not BOT_TOKEN:
+    bot.loop.run_until_complete(autobot())
+idle()
+if len(sys.argv) not in (1, 3, 4):
     bot.disconnect()
 else:
     bot.run_until_disconnected()
